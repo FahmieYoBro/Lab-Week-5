eef59e9bac75cc48d486abfbdfcc924a30ca5203
FahmieYoBro <fyb7920@gmail.com>
2021-04-22 00:06:34 +0800

    Lab 5

---
+++ Lab 5/src/Exe2/Validation.java
@@ -0,0 +1,36 @@
+package Exe2;
+
+import java.io.Serializable;
+
+import Exe1.ItemProduct;
+
+public class Validation  {
+	
+	
+	public String validateItemName(ItemProduct item)
+	{
+		
+		String result = "Valid Name. ", name = item.getName();
+		
+		for(int i=0; i<name.length();i++)
+		{
+		    
+			char ch = name.charAt(i);
+		    
+			// To check if the character is not alphabet or space
+			if (!(Character.isLetter(ch) || ch == ' ') )
+			{
+				
+				result="Invalid Name. ";
+				
+		    }
+		
+		}
+		return result;	
+	
+
+	}
+
+
+
+}
